722e26b4e30ee594cd3a4730e6020df3f1274239
Aleksandr Ivanenko <ayuivanenko@gmail.com>
2019-09-24 20:15:15 +0400

    api refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -1,33 +1,34 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "16aedc44-935d-40d8-a8b8-3857bd3be2f3"
+    }
+})
+
 export const getUsers = (count = 10, pageNumber = 1) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${count}&page=${pageNumber}`,
-        {withCredentials: true}
-    ).then(response => response.data);
+    return instance.get(`users?count=${count}&page=${pageNumber}`)
+        .then(response => response.data);
 };
 
 export const getProfile = (userId) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`,
-        {withCredentials: true}
-    ).then(response => response.data);
+    return instance.get(`profile/${userId}`)
+        .then(response => response.data);
 };
 
 export const getAuth = () => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,
-        {withCredentials: true}
-    ).then(response => response.data);
+    return instance.get(`auth/me`)
+        .then(response => response.data);
 };
 
 export const postFollow = (userId) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {}, {
-        withCredentials: true,
-        headers: {"API-KEY": "16aedc44-935d-40d8-a8b8-3857bd3be2f3"}
-    }).then(response => response.data);
+    return instance.post(`follow/${userId}`)
+        .then(response => response.data);
 };
 
 export const delFollow = (userId) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {
-        withCredentials: true,
-        headers: {"API-KEY": "16aedc44-935d-40d8-a8b8-3857bd3be2f3"}
-    }).then(response => response.data);
+    return instance.delete(`follow/${userId}`)
+        .then(response => response.data);
 };
